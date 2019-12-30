33ca4e98ea812bedfab75cdd9ccb0f343a60ec86
ullataponen <ulla.taponen@gmail.com>
2019-12-30 10:42:10 +0100

    Simplify props to carry only necessary information

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,51 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
+const Header = props => {
+	return (
+		<>
+			<h1> {props.name} </h1>
+		</>
+	);
+};
 
-const Content = (props) => {
-  return (
-    <div>
-      <p>{props.part} {props.exercise}</p>
-    </div>
-  )
-}
+const Part = props => {
+	return (
+		<>
+			<p>
+				{props.parts.name} {props.parts.exercises}
+			</p>
+		</>
+	);
+};
 
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.exercise}</p>
-    </div>
-  )
-}
+const Content = props => {
+	return (
+		<>
+			<Part parts={props.course[0]} />
+			<Part parts={props.course[1]} />
+			<Part parts={props.course[2]} />
+		</>
+	);
+};
+
+const Total = props => {
+	let sum =
+		props.exercises[0].exercises +
+		props.exercises[1].exercises +
+		props.exercises[2].exercises;
+	return (
+		<>
+			<p> Number of exercises {sum}</p>
+		</>
+	);
+};
 
 const App = () => {
-  const course = "Half Stack application development"
-  const part1 = "Fundamentals of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
+	const course = {
+		name: "Half Stack application development",
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7
+			},
+			{
+				name: "State of a component",
+				exercises: 14
+			}
+		]
+	};
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
-      <Total exercise={exercises1 + exercises2 + exercises3} />
-    </div>
-  )
-}
+	return (
+		<div>
+			<Header name={course.name} />
+			<Content course={course.parts} />
+			<Total exercises={course.parts} />
+		</div>
+	);
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
